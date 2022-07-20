5529976c6f79e52395c97189f024e79c34e560cf
daxkongjh <dax.k@live.com>
2022-07-20 13:22:24 +0800

    Update RecipeReviewCard.jsx

--- INCub8/src/Components/RecipeReviewCard.jsx
+++ INCub8/src/Components/RecipeReviewCard.jsx
@@ -24,16 +24,6 @@ import {
 } from "@mui/material";
 import CardModal from "./CardModal";
 
-// const ExpandMore = styled((props) => {
-//   const { expand, ...other } = props;
-//   return <IconButton {...other} />;
-// })(({ theme, expand }) => ({
-//   transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-//   marginLeft: "auto",
-//   transition: theme.transitions.create("transform", {
-//     duration: theme.transitions.duration.shortest,
-//   }),
-// }));
 
 export default function RecipeReviewCard({ dish, menuShortlist, setMenuShortlist }) {
   const [listCheck, setListCheck] = useState()
